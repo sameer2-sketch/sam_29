3d762d1d467c95e6f4d868443003fb92efa9f469
sameer2-sketch <mohammedsameer46911@gmail.com>
2024-06-22 22:46:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('proposalBtn').addEventListener('click', function() {
+    document.getElementById('proposalMessage').classList.remove('hidden');
+  });
+  alert("welcome to my heart")
